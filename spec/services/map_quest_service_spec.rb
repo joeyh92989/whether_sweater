25688300e493ae3d86e8881f9b7770b19e91a3ef
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-15 13:17:00 -0600

    setup mapquest service to return directions

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -15,6 +15,28 @@ RSpec.describe 'Map Quest Service' do
     describe 'sad path' do
       xit 'returns error if no location provided', :vcr do 
         results = MapQuestService.coordinates('')
+      end
+    end
+  end
+  describe 'return directions' do
+    describe 'happy path' do
+      it 'can return directions for a trip if provided valid start & end', :vcr do
+        results = MapQuestService.directions('denver,co', 'pueblo,co')
+        expect(results).to be_a(Hash)
+        expect(results).to have_key(:route)
+        expect(results[:route]).to have_key(:formattedTime)
+        expect(results[:route]).to have_key(:realTime)
+        expect(results[:route][:formattedTime]).to be_a(String)
+        expect(results[:route][:realTime]).to be_a(Integer)
+      end
+    end
+    describe 'sad path' do
+      it 'can return an error when the route selected is not viable', :vcr do
+        results = MapQuestService.directions('denver,co', 'london')
+        expect(results).to be_a(Hash)
+        expect(results).to have_key(:route)
+        expect(results[:route]).to have_key(:routeError)
+        expect(results[:info]).to have_key(:messages)
 
       end
     end
