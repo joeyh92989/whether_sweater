39bad5b9b454a0cd0c76b799e222b65a31071b34
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-15 15:21:32 -0600

    added sad path to facade

--- spec/facades/trip_facade_spec.rb
+++ spec/facades/trip_facade_spec.rb
@@ -1,13 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Trip Facade' do
-  it 'returns a weather hash similar to time of arrival', :vcr do
-    arrival_time = Time.now + 100950
-    response = TripFacade.get_weather_at_arrival("New York", arrival_time)
-    expect(response).to be_a(Hash)
-    expect(response).to have_key(:temperature)
-    expect(response[:temperature]).to be_a(Float)
-    expect(response).to have_key(:conditions)
-    expect(response[:conditions]).to be_a(String)
+  describe 'it can create a trip object' do
+    describe 'happy path' do
+      it 'returns a weather hash similar to time of arrival', :vcr do
+        arrival_time = Time.now + 100_950
+        response = TripFacade.get_weather_at_arrival('New York', arrival_time)
+        expect(response).to be_a(Hash)
+        expect(response).to have_key(:temperature)
+        expect(response[:temperature]).to be_a(Float)
+        expect(response).to have_key(:conditions)
+        expect(response[:conditions]).to be_a(String)
+      end
+
+      it 'returns a Roadtrip poro based on trip data', :vcr do
+        response = TripFacade.create_trip('Denver,CO', 'New York, NY')
+        expect(response).to be_a(Roadtrip)
+        expect(response.end_city).to eq('New York, NY')
+        expect(response.start_city).to eq('Denver,CO')
+        expect(response.travel_time).to be_a(String)
+        expect(response.weather_at_eta).to be_a(Hash)
+      end
+    end
+    describe 'sad path'do
+      it 'returns impossible travel time and no weather when travel is not viable', :vcr do 
+        response = TripFacade.create_trip('Denver,CO', 'London, England')
+        expect(response).to be_a(Roadtrip)
+        expect(response.end_city).to eq('London, England')
+        expect(response.start_city).to eq('Denver,CO')
+        expect(response.travel_time).to eq("impossible")
+        expect(response.weather_at_eta.empty?).to eq(true)
+      end
+    end
   end
 end
