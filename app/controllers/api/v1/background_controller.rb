a0aa8c9cb7e937e4d5601026c14500d5771332b0
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-15 22:00:13 -0600

    added better handling for background image

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::BackgroundController < ApplicationController
   def index
     background = UnsplashService.city_image(params[:location])
-    poro = Background.new(background)
-    render json: ImageSerializer.new(poro)
+    if background[:results] == []
+      render json: {errors: "No Image Found"}, status: :not_found
+    else
+      poro = Background.new(background)
+      render json: ImageSerializer.new(poro)
+    end
   end
 end
